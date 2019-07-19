f78fa9d0b00b15b07184d2be9cfcdddd0c29a8ce
juaiglesias <juan.manuel.iglesias93@gmail.com>
2019-07-19 17:07:26 -0300

    Skeleton repaired

---
+++ web-app/backend/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () =>  {
+    console.log(`Server is running on port ${port}`);
+});
