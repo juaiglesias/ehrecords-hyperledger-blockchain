2cd5becdadb2f55334d4875ab5d020b0c452081c
juaiglesias <juan.manuel.iglesias93@gmail.com>
2019-08-17 13:37:56 -0300

    Fixes in chaincode response

--- web-app/backend/controllers/patients.controller.js
+++ web-app/backend/controllers/patients.controller.js
@@ -47,10 +47,11 @@ exports.getPatient = async function(id) {
 
         // Evaluate the specified transaction.
         // getPatient transaction - requires arg ID
-        const result = await contract.evaluateTransaction('getPatient', id);
-        console.log(`Transaction has been evaluated, result is: ${result.toString()}`);
+        const result = await contract.evaluateTransaction('GetPatient', id);
 
-        return result;
+        response.patient = JSON.parse(result.toString());
+        console.log(`Transaction has been evaluated, result is: ${JSON.stringify(response)}`);
+        return response;
 
     } catch (error) {
         console.error(`Failed to evaluate transaction: ${error}`);
@@ -59,3 +60,93 @@ exports.getPatient = async function(id) {
     }
 
 } 
+
+exports.getAllPatients = async function() {
+    try {
+        var response = {};
+
+        // Create a new file system based wallet for managing identities.
+        const walletPath = path.join(process.cwd(), '/wallet');
+        const wallet = new FileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
+
+        // Check to see if we've already enrolled the user.
+        const userExists = await wallet.exists(userName);
+        if (!userExists) {
+            console.log('An identity for the user ' + userName + ' does not exist in the wallet');
+            console.log('Run the registerUser.js application before retrying');
+            response.error = 'An identity for the user ' + userName + ' does not exist in the wallet. Register ' + userName + ' first';
+            return response;            
+        }
+
+        // Create a new gateway for connecting to our peer node.
+        const gateway = new Gateway();
+        await gateway.connect(ccp, { wallet, identity: userName, discovery: gatewayDiscovery });
+
+        // Get the network (channel) our contract is deployed to.
+        const network = await gateway.getNetwork('ehrchannel');
+
+        // Get the contract from the network.
+        const contract = network.getContract('ehrecords');
+
+        // Evaluate the specified transaction.
+        // getPatient transaction - requires arg ID
+        const result = await contract.evaluateTransaction('GetAllPatients');
+        console.log(result.toString());
+        response.patients = JSON.parse(result.toString());
+        console.log(`Transaction has been evaluated, result is: ${JSON.stringify(response)}`);
+
+        return response;
+
+    } catch (error) {
+        console.error(`Failed to evaluate transaction: ${error}`);
+        response.error = error.message;
+        return response;
+    }
+
+}
+
+exports.createPatient = async function(key, firstName, lastName, age, address) {
+    try {
+        var response = {};
+
+        // Create a new file system based wallet for managing identities.
+        const walletPath = path.join(process.cwd(), '/wallet');
+        const wallet = new FileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
+
+        // Check to see if we've already enrolled the user.
+        const userExists = await wallet.exists(userName);
+        if (!userExists) {
+            console.log('An identity for the user ' + userName + ' does not exist in the wallet');
+            console.log('Run the registerUser.js application before retrying');
+            response.error = 'An identity for the user ' + userName + ' does not exist in the wallet. Register ' + userName + ' first';
+            return response;            
+        }
+
+        // Create a new gateway for connecting to our peer node.
+        const gateway = new Gateway();
+        await gateway.connect(ccp, { wallet, identity: userName, discovery: gatewayDiscovery });
+
+        // Get the network (channel) our contract is deployed to.
+        const network = await gateway.getNetwork('ehrchannel');
+
+        // Get the contract from the network.
+        const contract = network.getContract('ehrecords');
+
+        // Evaluate the specified transaction.
+        // getPatient transaction - requires arg ID
+        const result = await contract.submitTransaction('CreatePatient', key, firstName, lastName, age, address);
+        
+        response.newPatient = JSON.parse(result.toString());
+
+        console.log(`Transaction has been evaluated, result is: ${JSON.stringify(response)}`);
+
+        return response;
+
+    } catch (error) {
+        console.error(`Failed to evaluate transaction: ${error}`);
+        reponse.error = error.message;
+        return response;
+    }
+}
--- web-app/backend/routes/patients.routes.js
+++ web-app/backend/routes/patients.routes.js
@@ -2,17 +2,24 @@ const express = require('express');
 const router = express.Router();
 const patientsController = require('../controllers/patients.controller');
 
-router.get('/', (req, res) => {
+router.get('/', (_, res) => {
     patientsController.getAllPatients()
         .then((patients) => {
-            res.send(JSON.parse(patients))
+            res.json(patients);
         });
+});
+
+router.post('/', (req, res) => {
+    patientsController.createPatient(req.body.key, req.body.firstName, req.body.lastName, req.body.age, req.body.address)
+        .then((patient) => {
+            res.json(patient);
+        })
 });
 
 router.get('/:id', (req, res) => {
     patientsController.getPatient(req.params.id)
         .then((patient) => {
-            res.send(JSON.parse(patient))
+            res.json(patient);
         });
 })
 
